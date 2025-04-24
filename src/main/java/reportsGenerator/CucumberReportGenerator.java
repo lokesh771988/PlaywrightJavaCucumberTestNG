dff939af3b4ab2b89f21544975249a1ae38699df
lokesh771988 <lokesh.ust@gmail.com>
2025-04-24 12:04:07 +0530

    updating code

--- src/main/java/reportsGenerator/CucumberReportGenerator.java
+++ src/main/java/reportsGenerator/CucumberReportGenerator.java
@@ -4,17 +4,17 @@ import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class CucumberReportGenerator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File reportOutputDirectory = new File("target/cucumber-report-html");
-        List<String> jsonFiles = Arrays.asList("target/cucumber.json");
+		File reportOutputDirectory = new File("target/cucumber-reports");
+        List<String> jsonFiles = Collections.singletonList("target/cucumber.json");
 
-        Configuration config = new Configuration(reportOutputDirectory, "Playwright Java Cucumber");
+        Configuration config = new Configuration(reportOutputDirectory, "PlaywrightJavaCucumberTestNG");
         ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, config);
         reportBuilder.generateReports();
 	}
