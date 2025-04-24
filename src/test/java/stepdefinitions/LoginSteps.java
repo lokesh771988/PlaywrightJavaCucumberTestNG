1fa029b7984af087973f08a023e26f68b61140d5
lokesh771988 <lokesh.ust@gmail.com>
2025-04-24 10:49:14 +0530

    modifying browser context

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -27,7 +27,7 @@ public class LoginSteps {
 	@Given("user lanches the application")
 	public void lanuchApp() {
 		playwright = Playwright.create();
-		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
 		context = browser.newContext();
         page = context.newPage();
         page.navigate("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
